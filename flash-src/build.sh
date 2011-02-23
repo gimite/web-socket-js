4439ab0133af11d040a259ac6ce78b3e554253c0
Simeon Bateman <simeon@simb.net>
2011-02-22 18:16:34 -0800

    updated build script to create WebSocket.swc library file

--- flash-src/build.sh
+++ flash-src/build.sh
@@ -5,6 +5,9 @@
 
 mxmlc -static-link-runtime-shared-libraries -target-player=10.0.0 -output=../WebSocketMain.swf WebSocketMain.as &&
 mxmlc -static-link-runtime-shared-libraries -output=../WebSocketMainInsecure.swf WebSocketMainInsecure.as &&
+compc -source-path . -include-classes WebSocket IWebSocketWrapper -output=../WebSocket.swc &&
 cd .. &&
-zip WebSocketMainInsecure.zip WebSocketMainInsecure.swf &&
-rm WebSocketMainInsecure.swf
+zip WebSocketMainInsecure.zip WebSocketMainInsecure.swf WebSocket.swc &&
+rm WebSocketMainInsecure.swf &&
+rm WebSocket.swc
+
